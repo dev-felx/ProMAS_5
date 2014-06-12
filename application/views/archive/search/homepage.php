ca1f9ae6ca9f603444443b1dad932e2878b08a4d
User <User@Junior>
2014-06-12 17:56:08 +0300

    Merge origin/master
    Conflicts:
    	application/config/autoload.php
    	application/config/config.php
    	application/controllers/access/login.php
    	application/controllers/access/password.php
    	application/controllers/home.php
    	application/helpers/common_helper.php
    	application/models/access/manage_users.php
    	application/views/templates/header.php
    	application/views/templates/side_bar.php
    	assets/css/bootstrap_tweaks.css
    	assets/css/main.css

--- application/views/archive/search/homepage.php
+++ application/views/archive/search/homepage.php
@@ -5,7 +5,9 @@
                 <div class="jumbotron" style="background-color: #BCE8F1;">
                     <div class="container">
                         <h1 style="color: #0093D0;">sProMAS Archive</h1>
-                        <p>a bit details about sProMAS Archive bla bla blab lablablablab lblaba lbala lbablba lbalab lbalbal abalba labablba labalba lbaalb</p>
+                        <p>Welcome to students Project Management and Archiving System (sProMAS) Archive.<br>
+                        You can look up and explore projects done by final year students at the College of Informatics and Communication Technology (CoICT)
+                        of the University of Dar es Salaam. All information about projects participants and related documents is available here.</p>
                         <p><a href="<?php echo site_url('archive/archive/explore'); ?>" class="btn btn-primary btn-lg" role="button">Explore Projects</a>
                             <a href="<?php echo site_url('archive/archive/search');?>" class="btn btn-primary btn-lg pull-right" role="button">Search Projects</a></p>
                     </div>
