38e425bf9c2deee266f4e5f6c7bedadf70dbb0c4
Tesha-E <greenbug333.ez@gmail.com>
2014-06-13 13:59:05 +0300

    groups update

--- application/controllers/manage_users/group.php
+++ application/controllers/manage_users/group.php
@@ -19,6 +19,11 @@ class Group extends CI_Controller{
         //prepare data
         $data['projects'] = $this->project_model->get_all_project(array('space_id'=>$this->session->userdata('space_id')));
         
+        //get additional data
+       $data['students_add'] = $this->manage_users->get_student(array('space_id'=>$this->session->userdata('space_id')));
+       $data['supers_add'] = $this->manage_users->get_non_student(array('non_student_users.space_id'=>$this->session->userdata('space_id'),'roles.role'=> 'supervisor'));
+       $data['panels_add'] = $this->manage_users->get_non_student(array('non_student_users.space_id'=>$this->session->userdata('space_id'),'roles.role'=> 'panel_head'));
+        
         //prepare views
         $data['views'] = array('manage_users/group_view');
         $data['sub_title'] = 'Manage Project Groups';
@@ -27,19 +32,37 @@ class Group extends CI_Controller{
     
     
    public function get_grp_details(){
-       $id = 6;
+       $id = $_POST['id'];
        $response['students'] = $this->manage_users->get_student(array('project_id'=>$id));
        
-       //get project data
+       //get project data and supervisor
        $project = $this->project_model->get_project_row($id);
        $response['super'] = $this->manage_users->get_non_student(array('non_student_users.user_id'=>$project['supervisor_id']));
        
-       
+       //get the panel haed
        $panel_form = $this->assessment_model->get_panel_head($id);
        $response['panel'] = $this->manage_users->get_non_student(array('non_student_users.user_id'=>$panel_form['owner'],'roles.role'=> 'panel_head'));
-       
+         
        $response['status'] = 'true';
        header('Content-type: application/json');
        exit(json_encode($response));
+   }
+   
+   public function remove_stu(){
+       $user_id = $_POST['id'];
+       $data = array(
+           'project_id' => null,
+           'group_no' => null
+       );
+       $res = $this->manage_users->update_student($user_id,$data);
+       if($res){
+            $response['status'] = 'true';
+            header('Content-type: application/json');
+            exit(json_encode($response));
+       }
+   }
+   
+   public function add_stu(){
+       
    }
 }
--- application/views/manage_users/group_view.php
+++ application/views/manage_users/group_view.php
@@ -22,11 +22,36 @@
                             }
                         ?>
                     </select>
+                    
                 </div>
-                <div class="form-group hidden" id="stu_wrap">
-                     <label class="control-label">Students</label>
-                    <ul id="stud" class="list-group">
-                    </ul>
+                <div id='msg_frm'></div>
+                <div class="form-group hidden detail" >
+                    <label class="control-label">Students</label>
+                    <ul id="stud" class="list-group"></ul>
+                    <button id='show_stu_add' class="btn btn-primary btn-sm col-sm-2"><span class="glyphicon glyphicon-plus"></span>Add Student</button>
+                    <div class="hidden" id='stu_add_cont'>
+                    <div class="col-sm-7">
+                        <select id="stu_add_val" class="form-control col-sm-7">
+                            <?php
+                                foreach ($students_add as $value) {
+                                   echo '<option value="'.$value['student_id'].'">'.$value[first_name].' '.$value['last_name'].' Group ';
+                                   echo $value['group_no'];
+                                   echo '</option>';
+                                }
+                            ?>
+                        </select>  
+                    </div>
+                    <button id='stu_add_now' class="btn btn-success btn-sm col-sm-2">Add</button>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+                <div class="form-group hidden detail">
+                    <label>Project Supervisor<span><button class="btn btn-link btn-sm"><span class="glyphicon glyphicon-refresh push_right_bit"></span>Change</button></span></label>
+                    <p id="super" class="form-control-static"></p>
+                </div>
+                <div class="form-group hidden detail">
+                    <label>Project Assessment Panel Head<span><button class="btn btn-link btn-sm"><span class="glyphicon glyphicon-refresh push_right_bit"></span>Change</button></span></label>
+                    <p id='panel' class="form-control-static"></p>
                 </div>
             </form>
         </div>
@@ -37,22 +62,51 @@
         //get project Info
         $('#project').change(function(){
             var id = $(this).val();
+            $('.detail').addClass('hidden'); 
+            $('#msg_frm').show();
+            $('#msg_frm').html('<img style="height: 30px;" class="col-sm-offset-5 push_right_bit" src="<?php echo base_url(); ?>/assets/images/ajax-loader.gif">Fetching Details....');
             setTimeout(function(){
                  var t = "<?php echo site_url(); ?>";
                  var c = t+"/manage_users/group/get_grp_details";
                  $.post( c, {id: id}).done(function(data) {
                      if(data.status === 'true'){
+                        //hide loader
+                        $('#msg_frm').hide();
                         //populate students
+                        $('#stud').html('');
                         for(var i = 0; i < data['students'].length; i++){
                             var x = data['students'][i].first_name+" "+data['students'][i].last_name +" - "+ data['students'][i].registration_no;
-                            $('#stud').append('<li id="'+data['students'][i].registration_no+'" class="stu_btn list-group-item">'+x+'</li>');
+                            $('#stud').append('<li id="'+data['students'][i].student_id+'" class="stu_btn list-group-item">'+x+'<span class="remove text-danger glyphicon glyphicon-remove pull-right"><span></li>');
                          }
-                         $('#stu_wrap').removeClass('hidden');
-                         
-                         
+                         $('#panel').html(data.panel[0].first_name+' '+data.panel[0].last_name);
+                         $('#super').html(data.panel[0].first_name+' '+data.panel[0].last_name);      
+                         $('.detail').removeClass('hidden');   
+                     }else{
+                        $('#msg_frm').html('<div class="alert alert-danger">Error Fetching Data</div>');
                      }
                  },'json');
              },400);
         });
+        
+         $('body').on('click', '.remove', function () {
+            var id = $(this).parent('li').attr('id');
+            if(confirm('Remove student from this project')){
+                var t = "<?php echo site_url(); ?>";
+                 var c = t+"/manage_users/group/remove_stu";
+                 $.post( c, {id: id}).done(function(data) {
+                     if(data.status === 'true'){
+                         $('#project').trigger('change');
+                     }
+                 },'json');
+            }
+         });
+         
+         
+         $('#show_stu_add').click(function(){
+            $('#stu_add_cont').removeClass('hidden');
+            return false;
+         });
+         
+         $('#')
     });
 </script>
